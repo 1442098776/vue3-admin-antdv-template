1a822b74bff8bcb3158c4fe0cf9b57a98a99ce3f
黄展强 <huangzhanqiang@huangzhanqiangdeMacBook-Pro.local>
2024-04-22 23:45:25 +0800

    feat: antdv定制主题和国际化

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
-<script setup></script>
+<script setup>
+// 导入中文包
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
+const theme = {
+  token: {
+    colorPrimary: '#e15536',
+  },
+}
+</script>
 
 <template>
-  <div></div>
-  <a-button type="primary">主色按扭</a-button>
-  <StepBackwardOutlined />
-  <StepForwardOutlined />
+  <!--组件库全局配置-->
+  <a-config-provider :theme="theme" :locale="zhCN">
+    <a-button type="primary">主色按扭</a-button>
+    <StepBackwardOutlined />
+    <StepForwardOutlined />
+
+    <a-pagination :total="500" show-quick-jumper />
+  </a-config-provider>
 </template>
 
 <style lang="" scoped></style>
