38a12fcf9817d3dcfdc2b66adc27d1d3c6d9b32b
黄展强 <huangzhanqiang@huangzhanqiangdeMacBook-Pro.local>
2024-05-22 21:22:48 +0800

    feat: 加载进度条和标题设置

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+NProgress.configure({
+  showSpinner: false,
+})
 
 const router = createRouter({
   history: createWebHistory(),
@@ -7,13 +13,31 @@ const router = createRouter({
       path: '/',
       name: 'Home',
       component: () => import('@/views/Layout/index.vue'),
+      meta: {
+        title: '首页',
+      },
     },
     {
       path: '/login',
       name: 'login',
       component: () => import('@/views/Login/index.vue'),
+      meta: {
+        title: '登录页',
+      },
     },
   ],
+})
+
+router.beforeEach(() => {
+  // 进度条开始
+  NProgress.start()
+})
+
+router.afterEach((to) => {
+  // 进度条结束
+  NProgress.done()
+  // 动态设置标题
+  document.title = `${to.meta.title || import.meta.env.VITE_APP_TITLE}`
 })
 
 export default router
